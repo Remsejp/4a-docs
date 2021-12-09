ba6ad104a8b7192a0ba226e70a766bd280731099
MiguelFeles <78934114+MiguelFeles@users.noreply.github.com>
2021-12-08 22:45:48 -0500

    User Auth Microservicio

---
+++ auth_ms/authApp/views/__init__.py
@@ -0,0 +1,6 @@
+from .userCreateView import UserCreateView
+from .userDetailView import UserDetailView
+
+from .userCreateView import UserCreateView
+from .userDetailView import UserDetailView
+from .verifyTokenView import VerifyTokenView
